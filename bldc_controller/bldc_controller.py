832e7b3cdfe7378cfd1b55a16baaf4dccc181aa3
Michael Green <michael.green@orangesquirrel.net>
2021-02-05 02:58:55 +0000

    added channel for serial to mdc100

--- bldc_controller/bldc_controller.py
+++ bldc_controller/bldc_controller.py
@@ -13,59 +13,55 @@ from trio_serial import SerialStream
 
 from packages.shutdown_manager import ShutdownManager
 
-async def serial_manager(shutdown_manager, receive_command):
-    logger.debug(f'serial_manager starting')
-    with shutdown_manager.cancel_on_shutdown():
-        # async with SerialStream(
-        #     port=mdc100_port,
-        #     baudrate=38400,
-        #     xonxoff=True
-        # ) as serial:
-        async with receive_command:
-            async for command in receive_command:
-                logger.debug(f'serial_manager got command: {command}')
-    if shutdown_manager.shutting_down:
-        logger.debug(f'serial_manager stopped')
-        return
 
-
-async def process_messages(mdc100_port, shutdown_manager, send_command):
-    logger.debug(f'process_messages starting')
+async def mdc100_manager(shutdown_manager, receive_command):
+    logger.debug(f'mdc100_manager starting')
     with shutdown_manager.cancel_on_shutdown():
         async with SerialStream(
             port=mdc100_port,
             baudrate=38400,
             xonxoff=True
         ) as serial:
-            async with mqtt_client('192.168.7.122', 1883, 60) as client:
-                result = client.subscribe("mdc100", qos=1)
-                logger.debug(f'result of client.subscribe: {result}')
-                async with send_command:
-                    async for message in client.messages():
-                        logger.debug(f'message: {message.payload.decode()}')
-                        json_dict = json.loads(message.payload.decode())
-                        commands = [json_dict["cmd"].encode()+b'\r']
-                        # Append a return code command to the request
-                        commands.append(b'@0!\r')
-                        logger.debug(f'command: {commands}')
-                        # commands = [b'@0V*\r', b'@0!\r']
-                        for command in commands:
-                            await serial.send_all(command)
-                            logger.debug(f'sent: {command.decode().rstrip()}')
-                            await send_command.send(f'{command.decode().rstrip()}')
-                            await trio.sleep(0.1)
-                        with trio.move_on_after(0.1) as cancel_scope:
-                            data = await serial.receive_some(70)
-                            logger.debug(f'got: {data.decode()!r}')
-                            logger.debug(f'with a length of {len(data)} bytes')
-                            output_list = data.split(b'\r')
-                            output_list = [element.decode('utf8')
-                                           for element in output_list]
-                            output_list = list(filter(None, output_list))
-                            logger.debug(f'returned list: {output_list!r}')
-                        if cancel_scope.cancelled_caught:
-                            logger.debug(
-                                f'serial read timed out after .1 seconds')
+            async with receive_command:
+                async for command in receive_command:
+                    logger.debug(
+                        f'mdc100_manager got command: {command.decode().rstrip()}')
+                    await serial.send_all(command)
+                    logger.debug(f'sent: {command.decode().rstrip()}')
+                    await trio.sleep(0.1)
+                    with trio.move_on_after(0.1) as cancel_scope:
+                        data = await serial.receive_some(70)
+                        logger.debug(f'got: {data.decode()!r}')
+                        logger.debug(f'with a length of {len(data)} bytes')
+                        output_list = data.split(b'\r')
+                        output_list = [element.decode(
+                            'utf8') for element in output_list]
+                        output_list = list(filter(None, output_list))
+                        logger.debug(f'returned list: {output_list!r}')
+                    if cancel_scope.cancelled_caught:
+                        logger.debug(
+                            f'serial read timed out after .1 seconds')
+    if shutdown_manager.shutting_down:
+        logger.debug(f'mdc100_manager stopped')
+        return
+
+
+async def process_messages(mdc100_port, shutdown_manager, send_command):
+    logger.debug(f'process_messages starting')
+    with shutdown_manager.cancel_on_shutdown():
+        async with mqtt_client('192.168.7.122', 1883, 60) as client:
+            result = client.subscribe("mdc100", qos=1)
+            logger.debug(f'result of client.subscribe: {result}')
+            async with send_command:
+                async for message in client.messages():
+                    logger.debug(f'message: {message.payload.decode()}')
+                    json_dict = json.loads(message.payload.decode())
+                    commands = [json_dict["cmd"].encode('utf-8')+b'\r']
+                    # Append a return code command to the request
+                    commands.append(b'@0!\r')
+                    logger.debug(f'command: {commands}')
+                    for command in commands:
+                        await send_command.send(command)
     if shutdown_manager.shutting_down:
         logger.debug(f'process_messaging stopped')
         return
@@ -95,13 +91,13 @@ async def open_nurseries():
 
 
 async def main(mdc100_port):
-    logger.info('trio-mqtt-serial started')
+    logger.info('bldc controller started')
     await quick_toggle('pinctrl-bcm2711', pin=25)
     shutdown_manager = ShutdownManager()
     async with open_nurseries() as nurseries:
         nursery, daemon = nurseries
         daemon.start_soon(shutdown_manager._listen_for_shutdown_signals)
-        send_command, receive_command = trio.open_memory_channel(0)
+        send_command, receive_command = trio.open_memory_channel(1)
         async with send_command, receive_command:
             # mqtt_sync = mqtt.Client()
             # client = AsyncClient(mqtt_sync, nursery)
@@ -110,8 +106,8 @@ async def main(mdc100_port):
             nursery.start_soon(process_messages, mdc100_port,
                                shutdown_manager, send_command.clone())
             nursery.start_soon(
-                serial_manager, shutdown_manager, receive_command.clone())
-    logger.info('trio-mqtt-serial stopped')
+                mdc100_manager, shutdown_manager, receive_command.clone())
+    logger.info('bldc controller stopped')
 
 
 if __name__ == '__main__':
